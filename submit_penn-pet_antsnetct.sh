4c3420b6bef0de41135926af1a086857c2108fc1
jeffrey-phillips <jefphi@pennmedicine.upenn.edu>
2026-02-11 00:20:29 -0500

    Altered logfile path in wrapper script

--- submit_penn-pet_antsnetct.sh
+++ submit_penn-pet_antsnetct.sh
@@ -11,9 +11,10 @@ cat ${infile} | while IFS="," read f t1; do
 	fp=(${fbase//_/\ })
 	subj=${fp[0]}
 	petsess=${fp[1]}
-	wd=/project/wolk_4/SCAN/bids/derivatives/pennpet/${subj}/${petsess}
+	wd=/project/ftdc_pipeline/data/pet/${subj}/${petsess}
+	#wd=/project/wolk_4/SCAN/bids/derivatives/pennpet/${subj}/${petsess}
 	if [[ ! -d ${wd} ]]; then mkdir -p ${wd}; fi
-	logstem=/project/wolk_4/SCAN/bids/derivatives/pennpet/logs/${subj}_${petsess}
+	logstem=/project/ftdc_pipeline/data/pet/logs/${subj}_${petsess}
 	cmd="bsub -J pennpet_${subj}_${petsess} -o ${logstem}_%J_log.txt -n 1 ${scriptdir}/penn-pet_antsnetct.sh ${f} ${t1} ${wd}"
 	echo $cmd
 	$cmd
